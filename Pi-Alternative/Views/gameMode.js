fd75bc3c5d143e4d9e0331df92b9068eb20a948d
bjornskole <84008893+bjornskole@users.noreply.github.com>
2021-09-01 10:42:10 +0200

    Add files via upload

---
+++ Pi-Alternative/Views/gameMode.js
@@ -0,0 +1,6 @@
+let gameboardHTML = () => `
+   <div>Game</div>
+   <div>${pi.decimalsStr}</div>
+   <input onchange="pi.get(0)" />
+   `
+
---
+++ Pi-Alternative/Views/leaderboard.js
@@ -0,0 +1,7 @@
+
+let score = (item) => item.map(i => `<div>${i.playerName}-${i.highscore}</div>`)
+   
+let leaderboardHTML = () => `
+   <div>${score(model.data.players)}</div>
+   `
+
