6a0b8cff7b0b53c9b0e343245697e48203d594fd
Flashforwarcl <asb.helgen@gmail.com>
2021-09-01 12:45:11 +0200

    updates

--- Pi-Alternative/Views/gameMode.js
+++ Pi-Alternative/Views/gameMode.js
@@ -1,9 +1,7 @@
 let gameHTML = () => `
-    <input onchange="setPlayerName(this.value)" type="text" id="inputPlayer">
-     <select onchange="" name="" >
-       <option value="Top5">${model.gameModes.mode[0]}</option>
-       <option value="Graph">${model.gameModes.mode[1]}</option>
+   <select onchange="" name="" >
+      <option value="Top5">${model.gameModes.mode[0]}</option>
+      <option value="Graph">${model.gameModes.mode[1]}</option>
    </select>
    <button onclick="changeView(gameboardHTML)">Start Game</button>
-   <div>game-site</div>
    `;
