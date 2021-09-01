5ea685c58ed68fd76cc533775b9778f6827c8cc2
bjornskole <84008893+bjornskole@users.noreply.github.com>
2021-09-01 10:44:47 +0200

    Add files via upload

---
+++ Pi-Alternative/Views/statistics.js
@@ -0,0 +1,9 @@
+
+let statisticsHTML = () => `
+   <div>statistics</div>
+   <select onchange="" name="cars" id="cars">
+       <option value="Top5">${model.statistics.type[0]}</option>
+       <option value="Graph">${model.statistics.type[1]}</option>
+   </select>
+   `
+   
