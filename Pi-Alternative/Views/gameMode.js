84c42a0ff5b21de4b2f42fa8fdb1f3e2467c1667
Flashforwarcl <asb.helgen@gmail.com>
2021-09-09 11:35:10 +0200

    Update gameMode.js

--- Pi-Alternative/Views/gameMode.js
+++ Pi-Alternative/Views/gameMode.js
@@ -1,5 +1,6 @@
 let gameHTML = () => `
    <select onchange="setGameMode(this.value)">
+      <option selected="${model.gameModes.selected}">${model.gameModes.selected}</option>
       <option value="${model.gameModes.mode[0]}">${model.gameModes.mode[0]}</option>
       <option value="${model.gameModes.mode[1]}">${model.gameModes.mode[1]}</option>
    </select>
