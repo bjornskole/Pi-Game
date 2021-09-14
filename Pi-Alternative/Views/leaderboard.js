c6d31b3f268e172c34d7d4857ba58b9686515027
Flashforwarcl <asb.helgen@gmail.com>
2021-09-14 11:34:13 +0200

    Update leaderboard.js

--- Pi-Alternative/Views/leaderboard.js
+++ Pi-Alternative/Views/leaderboard.js
@@ -7,7 +7,6 @@ let leaderboardHTML = () => `
   <button onclick="gamemodeBack(leaderboardHTML)"><-</button>
   ${model.gameModes.selected}
   <button onclick="gamemodeForward(leaderboardHTML)">-></button>
-  <div>Player Date Time Score</div>
   <div>${setLeaderboardType(model.gameModes.selected)}</div>
 `;
 
