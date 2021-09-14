87424d2bf11196eaa6c075174629f93f3dee9ce6
Adrian <adgrjb@gmail.com>
2021-09-14 14:56:14 +0200

    need to sort top 5 games based on mode

--- Pi-Alternative/Views/statistics.js
+++ Pi-Alternative/Views/statistics.js
@@ -11,12 +11,20 @@ let statisticsHTML = () => `
    </select>
    <div>${model.statistics.selected === "Top5" ? Top5() : Graph()}</div>
    `;
-let Top5 = function () {
-  return `Top5<br>
-  <input onclick="this.value = ''" type="text" list="Playernames" value="${model.main.playerName}"/>
-    <datalist id="Playernames">
-    ${model.statistics.playerNames}
-    </datalist>
+   let Top5 = function (val) {
+    let tmp2 = [];
+    let tmp = [];
+    for (let i = 0; i < model.data.players.length; i++) {
+        if (model.data.players[i].playerName === val) {
+          tmp.push(model.data.players[i]);
+          for (j = 0; j < model.data.gamesPlayed.length; j++){
+            tmp2.push(model.data.gamesPlayed.find(({playerId})=> playerId === tmp[0].playerId));
+          }
+        }
+        console.log('tmp:' + tmp + 'tmp2:' + tmp2);
+      }
+      //console.log(tmp);
+  return `Top5
     `;
 };
 
