349f2936dd29a1ae8facf8596363676043635160
Flashforwarcl <asb.helgen@gmail.com>
2021-09-14 11:36:14 +0200

    update

--- Pi-Alternative/Views/leaderboard.js
+++ Pi-Alternative/Views/leaderboard.js
@@ -2,7 +2,6 @@ let score = (item) =>
   item.map((i) => `<div>${i.playerName}-${i.highscore}</div>`);
 
 let leaderboardHTML = () => `
-
   <div>Leaderboard</div>
   <button onclick="gamemodeBack(leaderboardHTML)"><-</button>
   ${model.gameModes.selected}
