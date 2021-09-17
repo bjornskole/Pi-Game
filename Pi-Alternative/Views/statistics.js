942d646d68466525227d95f8a535aa79c82681a5
TheodorFrisvold <theodorfrisvoldgit@gmail.com>
2021-09-17 13:31:52 +0200

    added graph stuff examplecode

--- Pi-Alternative/Views/statistics.js
+++ Pi-Alternative/Views/statistics.js
@@ -1,9 +1,8 @@
 let statisticsHTML = () => `
    <div>Statistics</div>
    <select onchange="setStatMode(this.value)">
-        <option selected="${model.statistics.selected}">${
-  model.statistics.selected
-}</option>
+        <option selected="${model.statistics.selected}">${model.statistics.selected
+  }</option>
         <option value="Top5">${model.statistics.type[0]}</option>
         <option value="Graph">${model.statistics.type[1]}</option>
    </select>
@@ -11,9 +10,8 @@ let statisticsHTML = () => `
    `;
 let Top5 = function () {
   return `
-  <input onclick="this.value = ''" onchange="setSelectedPlayer(this.value)" type="text" list="Playernames" value="${
-    model.statistics.selectedPlayer
-  }"/>
+  <input onclick="this.value = ''" onchange="setSelectedPlayer(this.value)" type="text" list="Playernames" value="${model.statistics.selectedPlayer
+    }"/>
     <datalist id="Playernames">
       ${genPlayerList()}
     </datalist>
@@ -21,11 +19,47 @@ let Top5 = function () {
     ${model.statistics.top5list}
     `;
 };
-
+//start of graph stuff
 let Graph = function () {
   return `Graph
+  <div>
+  <canvas id="myChart"></canvas>
+  </div>
     `;
 };
+
+
+const labels = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+];
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'My First dataset',
+    backgroundColor: 'rgb(255, 99, 132)',
+    borderColor: 'rgb(255, 99, 132)',
+    data: [0, 10, 5, 2, 20, 30, 45],
+  }]
+};
+
+const config = {
+  type: 'line',
+  data: data,
+  options: {}
+};
+
+var myChart = new Chart(
+  document.getElementById('myChart'),
+  config
+);
+
+//end of graph stuff
+
 
 function setStatMode(val) {
   model.statistics.selected = val;
