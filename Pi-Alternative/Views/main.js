6de73094fe697f087a5f64dc39d553e2b6246d41
bjornskole <[bjorns@getacademy.no]>
2021-09-20 10:20:12 +0200

    Bjorns

--- Pi-Alternative/Views/main.js
+++ Pi-Alternative/Views/main.js
@@ -1,7 +1,9 @@
 let mainView = () => `
+<div class="mainContainer">
 <div>Main Site</div>
 <input id="inputBox"placeholder="Player name" onchange="setPlayerName(this.value)" type="text" id="inputPlayer"></br>
 <button onclick="changeView(gameHTML)">Game</button>
 <button onclick="changeView(statisticsHTML)">Statistics</button>
 <button onclick="changeView(leaderboardHTML)">LeaderBoard</button>
+</div>
 `;
